557debcc5e7d827bc3bf31ce1d84403601f97ea0
shadolite <amy.kawaii@gmail.com>
2023-03-09 16:16:43 -0700

    Added components for editing details and measurements

---
+++ app/src/pages/individual/editModal/editDetails.tsx
@@ -0,0 +1,91 @@
+import { IDetails } from "../../../models/individual/IDetails";
+import * as React from "react";
+import { Box, TextField } from "@mui/material";
+import Item from "../../../core/components/item";
+import MeasurementFormControl from "./measurements";
+
+interface Props {
+  details: IDetails;
+}
+
+const EditDetails: React.FunctionComponent<Props> = (
+  props: Props
+): JSX.Element => {
+  return (
+    <React.Fragment>
+      <Box
+        component="form"
+        sx={{
+          "& .MuiTextField-root": { m: 1 },
+        }}
+        noValidate
+        autoComplete="off">
+        <Item>
+          <TextField
+            id="givenName"
+            label="Given Name"
+            value={props.details.givenName}
+          />
+          <TextField
+            id="familyName"
+            label="Family Name"
+            value={props.details.familyName}
+          />
+          <div />
+          <TextField
+            id="birthDay"
+            label="Day"
+            type="date"
+            value={props.details.birthdate}
+          />
+          {/* <TextField
+            id="birthMonth"
+            label="Month"
+            type="month"
+            value={props.details.birthdate?.getMonth()}
+          />
+          <TextField
+            id="birthYear"
+            label="Year"
+            type="date"
+            value={props.details.birthdate?.getFullYear()}
+          /> */}
+          <MeasurementFormControl
+            centimeters={props.details.height}
+            kilograms={props.details.weight}
+          />
+          <TextField
+            id="bloodType"
+            label="Blood Type"
+            value={props.details.bloodType}
+          />
+          <TextField
+            id="address"
+            label="Address"
+            value={props.details.address}
+          />
+          <TextField
+            id="phoneNumber"
+            label="Phone Number"
+            value={props.details.phoneNumber}
+          />
+          <div>
+            <TextField
+              id="notes"
+              label="Notes"
+              fullWidth
+              multiline
+              sx={{
+                paddingRight: 2,
+              }}
+              value={props.details.notes}
+            />
+          </div>
+        </Item>
+        <Item></Item>
+      </Box>
+    </React.Fragment>
+  );
+};
+
+export default EditDetails;
---
+++ app/src/pages/individual/editModal/measurements.tsx
@@ -0,0 +1,163 @@
+import * as React from "react";
+import { FormControl, FormGroup, FormLabel, TextField } from "@mui/material";
+import ToggleButton from "@mui/material/ToggleButton";
+import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
+import * as ConversionHelper from "../../../utilities/conversionHelper";
+
+interface Props {
+  centimeters?: number;
+  kilograms?: number;
+}
+
+const MeasurementFormControl: React.FunctionComponent<Props> = (
+  props: Props
+): JSX.Element => {
+  const [measurementType, setMeasurementType] = React.useState<string | null>(
+    "metric"
+  );
+
+  // Imperial measurement values
+  // Height
+  const [feet, setFeet] = React.useState<number | undefined>(
+    ConversionHelper.getImperialHeight(props.centimeters)?.feet
+  );
+  const [inches, setInches] = React.useState<number | undefined>(
+    ConversionHelper.getImperialHeight(props.centimeters)?.inches
+  );
+  // Weight
+  const [pounds, setPounds] = React.useState<number | undefined>(
+    ConversionHelper.getImperialWeight(props.kilograms)?.pounds
+  );
+  const [ounces, setOunces] = React.useState<number | undefined>(
+    ConversionHelper.getImperialWeight(props.kilograms)?.ounces
+  );
+
+  // Measurement type selection handler
+  const handleMeasurementType = (
+    event: React.MouseEvent<HTMLElement>,
+    newMeasurement: string | null
+  ) => {
+    setMeasurementType(newMeasurement);
+  };
+
+  // Imperial measurement handlers
+  // Height
+  const handleFeet = (
+    event: React.FocusEvent<HTMLTextAreaElement | HTMLInputElement>
+  ) => {
+    setFeet(Number(event.target.value));
+    props.centimeters = ConversionHelper.getMetricHeight(feet, inches);
+  };
+  const handleInches = (
+    event: React.FocusEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    setInches(Number(event.target.value));
+    props.centimeters = ConversionHelper.getMetricHeight(feet, inches);
+  };
+  // Weight
+  const handlePounds = (
+    event: React.FocusEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    setPounds(Number(event.target.value));
+    props.kilograms = ConversionHelper.getMetricWeight(pounds, ounces);
+  };
+  const handleOunces = (
+    event: React.FocusEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    setOunces(Number(event.target.value));
+    props.kilograms = ConversionHelper.getMetricWeight(pounds, ounces);
+  };
+
+  // Metric measurement handlers
+  // Height
+  const handleCentimeters = (
+    event: React.FocusEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    let imperialHeight = ConversionHelper.getImperialHeight(
+      Number(event.target.value)
+    );
+    setFeet(imperialHeight?.feet);
+    setInches(imperialHeight?.inches);
+  };
+  // Weight
+  const handleKilograms = (
+    event: React.FocusEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    let imperialWeight = ConversionHelper.getImperialWeight(
+      Number(event.target.value)
+    );
+    setPounds(imperialWeight?.pounds);
+    setOunces(imperialWeight?.ounces);
+  };
+
+  return (
+    <FormControl component="fieldset">
+      // Measurement type toggle
+      <ToggleButtonGroup
+        value={measurementType}
+        exclusive
+        onChange={handleMeasurementType}
+        aria-label="text alignment">
+        <ToggleButton value="metric" aria-label="metric height">
+          Metric
+        </ToggleButton>
+        <ToggleButton value="imperial" aria-label="imperial height">
+          Imperial
+        </ToggleButton>
+      </ToggleButtonGroup>
+      {measurementType == "metric" ? (
+        // Metric fields
+        <React.Fragment>
+          <TextField
+            id="centimeters"
+            label="Height"
+            value={props.centimeters}
+            onBlur={handleCentimeters}
+          />
+          <TextField
+            id="kilograms"
+            label="Weight"
+            value={props.kilograms}
+            onBlur={handleKilograms}
+          />
+        </React.Fragment>
+      ) : (
+        // Imperial fields
+        <React.Fragment>
+          <FormLabel component="label">Height</FormLabel>
+          <FormGroup row>
+            <TextField
+              id="feet"
+              label="Feet"
+              value={feet}
+              onBlur={handleFeet}
+            />
+            <TextField
+              id="inches"
+              label="Inches"
+              value={inches}
+              onBlur={handleInches}
+            />
+          </FormGroup>
+          <FormLabel component="label">Weight</FormLabel>
+          <FormGroup row>
+            <TextField
+              id="pounds"
+              label="Pounds"
+              value={pounds}
+              onBlur={handlePounds}
+            />
+            <TextField
+              id="ounces"
+              label="Ounces"
+              value={ounces}
+              onBlur={handleOunces}
+            />
+          </FormGroup>
+        </React.Fragment>
+      )}
+    </FormControl>
+  );
+};
+
+export default MeasurementFormControl;
